45a502b0d041f189f598d5345b1e67506b37c88c
shengonglin <shengonglin@gmail.com>
2026-08-16 20:05:36 +0800

    更新 local.sh

--- local.sh
+++ local.sh
@@ -57,7 +57,7 @@ if [[ -n "$(echo "${PATH}" |grep -i 'windows')" ]]; then
     read -p "请选择：" YONU
     case ${YONU} in
     [Yy])
-        bash -c "$(curl -fsSL https://raw.githubusercontent.com/281677160/bendi/main/wsl.sh)"
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/shengonglin/bendi/main/wsl.sh)"
         exit 0
         break
     ;;
@@ -119,7 +119,7 @@ if [[ ! -f "/etc/oprelyonu" ]]; then
     ;;
     esac
   done
-  if sudo bash -c 'bash <(curl -fsSL https://github.com/281677160/common/raw/main/custom/ubuntu.sh)'; then
+  if sudo bash -c 'bash <(curl -fsSL https://github.com/shengonglin/common/raw/main/custom/ubuntu.sh)'; then
     sudo sh -c 'echo openwrt > /etc/oprelyonu'
   else
     sudo rm -rf /etc/oprelyo*
@@ -161,8 +161,8 @@ export MYCONFIG_FILE="${COMPILE_PATH}/seed/${CONFIG_FILE}"
 LINSHI_COMMON="/tmp/common"
 [[ ! -d "${OPERATES_PATH}" ]] && TIME r "缺少编译主文件,正在下载中..." || TIME y "正在执行：判断文件是否缺失"
 [[ -d "${LINSHI_COMMON}" ]] && rm -rf "${LINSHI_COMMON}"
-if ! git clone -q --single-branch --depth=1 --branch=main https://github.com/281677160/common "${LINSHI_COMMON}"; then
-  git clone --depth=1 https://github.com/281677160/common "${LINSHI_COMMON}"
+if ! git clone -q --single-branch --depth=1 --branch=main https://github.com/shengonglin/common "${LINSHI_COMMON}"; then
+  git clone --depth=1 https://github.com/shengonglin/common "${LINSHI_COMMON}"
 fi
 if [ -f "${LINSHI_COMMON}/custom/first.sh" ] && grep -qE "bash" "${LINSHI_COMMON}/custom/first.sh"; then
   chmod -R +x "${LINSHI_COMMON}"
@@ -725,7 +725,7 @@ done
 
 echo -e "\n${YELLOW}正在建立文件夹,请稍后...${NC}"
 sudo rm -rf /tmp/actions
-if git clone -q --depth 1 https://github.com/281677160/build-actions /tmp/actions; then
+if git clone -q --depth 1 https://github.com/shengonglin/build-actions /tmp/actions; then
   if [[ -d "${OPERATES_PATH}/${openwrt_wenjian}" ]]; then
     echo -e "${RED}错误：${openwrt_wenjian}文件夹已存在,无法再次建立！${NC}\n"
   else
